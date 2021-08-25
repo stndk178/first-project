46e57856b5557a578dc73f86af622bd0ba3533ba
kosuah <ksa8847@naver.com>
2021-08-26 01:09:20 +0900

    first commit

---
+++ c3/c3_2.java
@@ -0,0 +1,26 @@
+package c3;
+import java.util.*;
+import java.io.*;
+
+public class c3_2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int count = 0;
+        int max = arr[arr.length-1];
+        int second_max = arr[arr.length-2];
+        
+
+    }
+}
